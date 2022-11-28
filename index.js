c8ffecbb302afdb5b80cfe7471960b0a8daba05a
Ashutosh Kumar <104302587+Ashutosh-0101@users.noreply.github.com>
2022-11-28 22:10:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("anounsment").addEventListener('click', 
+function() {
+	alert("comming soon........")
+});
+document.getElementById("contact").addEventListener('click', 
+function() {
+	alert("kindly mail us on:- student.help.desk.0099@gmail.com")
+});
